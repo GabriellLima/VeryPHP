8603633dacba6e947cb649ab31084c354012640c
Robert San <robertsanseries@hotmail.com>
2015-12-26 15:56:43 -0300

    Classe model de exemplo

---
+++ app/models/ExemploModel.php
@@ -0,0 +1,21 @@
+<?php
+
+
+/**
+ * Classe ExemploModel
+ *
+ * Esta classe é apenas de exemplo.
+ * Toda classe Modelo criada tem que extender a classe Model por padrão.
+ *
+ * @package		VeryPHP
+ * @subpackage	app
+ * @category	controllers
+ * @author		Robert San
+ */
+
+class ExemploModel extends Model
+{
+
+	public function __construct(){}
+
+}
