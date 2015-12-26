1217580e45e2627bbf0ccff1e405b6dbf23db51a
Robert San <robertsanseries@hotmail.com>
2015-12-26 15:57:21 -0300

    View de exemplo

---
+++ app/views/Exemplo.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>VeryPHP</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <style type="text/css">
+        	*{
+        		margin: 0;
+        		padding: 0;
+        	}
+
+            html, body {
+                height: 100%;
+            }
+
+            #body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #fff;
+                display: table;
+                font-weight: 70;
+                font-family: 'Lato';
+                background-color: #0D47A1;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 40px;
+                margin-bottom: 40px;
+                padding: 2%;
+            }
+        </style>
+
+
+    </head>
+    <body id="body">
+        <div class="container">
+            <div class="content">
+                <div class="mdl-spinner mdl-js-spinner is-active"></div>
+                <div class="title">VeryPHP - View de Exemplo</div>
+            </div>
+        </div>
+    </body>
+</html>
