edb0ec0e7150e58ade7e7f7077a6208ad1b277a3
Robert San <robertsanseries@hotmail.com>
2015-12-26 15:59:18 -0300

    Arquivo de configuração de debug e timezone

---
+++ config/app.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+     | ------------------------------------------------- -------------------------
+     | Aplicação em Modo de Depuração
+     | ------------------------------------------------- -------------------------
+     |
+     | Quando seu aplicativo estiver em modo de depuração, mensagens de erro detalhadas com
+     | rastreamentos de pilha será mostrado em cada erro que ocorre dentro da sua
+     | aplicação. Se desativada, uma página de erro genérica simples é mostrado.
+     |
+     */
+
+    'debug' => env('APP_DEBUG', false),
+
+    /*
+     | ------------------------------------------------- -------------------------
+     | Fuso horário da aplicação
+     | ------------------------------------------------- -------------------------
+     |
+     | Aqui você pode especificar o fuso horário padrão para a sua aplicação, o que
+     | será utilizado pelas funções de data PHP e de data e hora.
+     |
+     */
+
+    'timezone' => 'America/Bahia',
+];
